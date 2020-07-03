e6ce1da0dfef1bfb0e345b75ab4600d7fc8b688f
Kikiodazie <kiki.odazie@yahoo.com>
2020-07-03 19:32:40 +0100

    Add all necessary business logic

--- src/main/java/com/odazie/jpa/learningjpa/business/service/UserService.java
+++ src/main/java/com/odazie/jpa/learningjpa/business/service/UserService.java
@@ -1,6 +1,30 @@
 package com.odazie.jpa.learningjpa.business.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.odazie.jpa.learningjpa.data.entity.UserRecord;
+import com.odazie.jpa.learningjpa.data.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public List<UserRecord> getAllUsers(){
+        List<UserRecord> userRecords = new ArrayList<>();
+        userRepository.findAll().forEach(userRecords::add);
+        return userRecords;
+    }
+
+    public void addUser(UserRecord userRecord){
+        userRepository.save(userRecord);
+    }
+
 }
--- src/main/java/com/odazie/jpa/learningjpa/web/UserController.java
+++ src/main/java/com/odazie/jpa/learningjpa/web/UserController.java
@@ -1,8 +1,27 @@
 package com.odazie.jpa.learningjpa.web;
 
 
+import com.odazie.jpa.learningjpa.business.service.UserService;
+import com.odazie.jpa.learningjpa.data.entity.UserRecord;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
+
+    private UserService userService;
+
+    @RequestMapping("/")
+    public List<UserRecord> getAllUser(){
+        return userService.getAllUsers();
+    }
+
+    @RequestMapping(value = "/add-user", method = RequestMethod.POST)
+    public void addUser(@RequestBody UserRecord userRecord){
+        userService.addUser(userRecord);
+    }
 }
