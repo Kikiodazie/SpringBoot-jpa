6bf702aef4d7258e1aaff077bf38a4892617b7a9
Kikiodazie <kiki.odazie@yahoo.com>
2020-07-03 19:10:33 +0100

    Create packages and EntityModel in POJO

---
+++ src/test/java/com/odazie/jpa/learningjpa/LearningJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odazie.jpa.learningjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
