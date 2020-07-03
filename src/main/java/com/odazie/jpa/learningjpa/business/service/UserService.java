cca84dbca78298b819e305b902027bab0fdeda82
Kikiodazie <kiki.odazie@yahoo.com>
2020-07-03 20:31:49 +0100

    refactor

--- src/main/java/com/odazie/jpa/learningjpa/business/service/UserService.java
+++ src/main/java/com/odazie/jpa/learningjpa/business/service/UserService.java
@@ -2,6 +2,7 @@ package com.odazie.jpa.learningjpa.business.service;
 
 import com.odazie.jpa.learningjpa.data.entity.UserRecord;
 import com.odazie.jpa.learningjpa.data.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,20 +11,17 @@ import java.util.List;
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-
-    public List<UserRecord> getAllUsers(){
-        List<UserRecord> userRecords = new ArrayList<>();
+    @Autowired
+    private UserRepository userRepository;
+    public List<UserRecord> getAllUsers()
+    {
+        List<UserRecord>userRecords = new ArrayList<>();
         userRepository.findAll().forEach(userRecords::add);
         return userRecords;
     }
-
-    public void addUser(UserRecord userRecord){
+    public void addUser(UserRecord userRecord)
+    {
         userRepository.save(userRecord);
     }
 
--- src/main/java/com/odazie/jpa/learningjpa/web/UserController.java
+++ src/main/java/com/odazie/jpa/learningjpa/web/UserController.java
@@ -3,6 +3,7 @@ package com.odazie.jpa.learningjpa.web;
 
 import com.odazie.jpa.learningjpa.business.service.UserService;
 import com.odazie.jpa.learningjpa.data.entity.UserRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,15 +14,16 @@ import java.util.List;
 @RestController
 public class UserController {
 
+    @Autowired
     private UserService userService;
-
     @RequestMapping("/")
-    public List<UserRecord> getAllUser(){
+    public List<UserRecord> getAllUser()
+    {
         return userService.getAllUsers();
     }
-
-    @RequestMapping(value = "/add-user", method = RequestMethod.POST)
-    public void addUser(@RequestBody UserRecord userRecord){
+    @RequestMapping(value="/add-user", method=RequestMethod.POST)
+    public void addUser(@RequestBody UserRecord userRecord)
+    {
         userService.addUser(userRecord);
     }
 }
